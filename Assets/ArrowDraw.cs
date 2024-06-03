55f174e725db5ea274ad49584b7fb0fb8b233c81
Apocalypse <k023g0063@g.neec.ac.jp>
2024-06-03 11:38:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/ArrowDraw.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowDraw : MonoBehaviour {
+    [SerializeField] 
+    private Image arrowImage;
+    private Vector3 clickPosition;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            arrowImage.gameObject.SetActive(true);
+            clickPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0)) {
+            Vector3 destination = clickPosition - Input.mousePosition;
+            Debug.Log(destination);
+
+            float size = destination.magnitude;
+
+            float angleRad = Mathf.Atan2(destination.y, destination.x);
+
+            arrowImage.rectTransform.position = clickPosition;
+            arrowImage.rectTransform.rotation = Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
+
+            arrowImage.rectTransform.sizeDelta = new Vector2(size, size);
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            arrowImage.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/PullingJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullingJump : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    private Vector3 clickPos;
+
+    [SerializeField] private float jumpPower = 10;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            clickPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            Vector3 dest = clickPos - Input.mousePosition;
+            if (dest.sqrMagnitude == 0) return;
+
+            rigidbody.velocity = dest.normalized * jumpPower;
+        }
+
+        if (transform.position.y < 10 && 0 < Physics.gravity.y) {
+            Physics.gravity = new Vector3(0, -9.8f, 0);
+        }
+        if (10 <= transform.position.y && Physics.gravity.y < 0) {
+            Physics.gravity = new Vector3(0, 9.8f, 0);
+        }
+    }
+}
