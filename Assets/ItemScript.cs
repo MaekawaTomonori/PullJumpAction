1a9cb56e2fe37402fe4b84eb6ab7f0ed7e96f269
Apocalypse <k023g0063@g.neec.ac.jp>
2024-06-10 12:57:22 +0900

    08 アニメーション

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour {
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+    
+    private void OnTriggerEnter(Collider other) {
+        animator.SetTrigger("Get");
+        //DestroySelf();
+    }
+
+    private void OnTriggerStay(Collider other) {
+    }
+
+    private void OnTriggerExit(Collider other) {
+    }
+
+    private void DestroySelf() {
+        Destroy(gameObject);
+    }
+}
--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PullingJump : MonoBehaviour {
 
     private Rigidbody rigidbody;
     private Vector3 clickPos;
+    private bool isCanJump;
 
-    [SerializeField] private float jumpPower = 10;
+    [SerializeField] private float jumpPower = 12;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,18 +21,34 @@ public class PullingJump : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)) {
             clickPos = Input.mousePosition;
         }
-        if (Input.GetMouseButtonUp(0)) {
+
+
+        if (isCanJump && Input.GetMouseButtonUp(0)) {
             Vector3 dest = clickPos - Input.mousePosition;
             if (dest.sqrMagnitude == 0) return;
 
             rigidbody.velocity = dest.normalized * jumpPower;
         }
 
-        if (transform.position.y < 10 && 0 < Physics.gravity.y) {
+        //ChangeGrav(遊び)
+        if (transform.position.y < 10) {
             Physics.gravity = new Vector3(0, -9.8f, 0);
-        }
-        if (10 <= transform.position.y && Physics.gravity.y < 0) {
+        }else if (10 <= transform.position.y) {
             Physics.gravity = new Vector3(0, 9.8f, 0);
         }
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        ContactPoint[] contacts = collision.contacts;
+        Vector3 otherNormal = contacts[0].normal;
+        Vector3 upVector = new Vector3(0, 1, 0);
+        float dotUN = Vector3.Dot(upVector, otherNormal);
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+        if (dotDeg <= 45) {
+            isCanJump = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision) {
+        isCanJump = false;
     }
 }
